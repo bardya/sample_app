7dafd20c8706c37179859e1e6fd03527d5335359
Bardya <Bardya@pionix.ca>
2013-09-29 00:28:31 -0700

    Add ApplicationHelper to RSpec and remove duplicate code from utilities

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,13 +1,1 @@
-
-# This is a duplicate of the helper defined in the ApplicationHelper module
-# Located at app/helpers/application_helper.rb but this is for testing purposes 
-# Refer to Listing 5.29 for details. [BA]
-
-def full_title(page_title)
-	base_title = "Ruby on Rails Tutorial Sample App"
-	if page_title.empty?
-		base_title
-	else
-		"#{base_title} | #{page_title}"
-	end
-end
+include ApplicationHelper
