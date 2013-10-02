2ba5bfb5ab37fcb0a892e452594da456a1099745
Bardya <Bardya@pionix.ca>
2013-10-02 01:14:21 -0700

    Add better code for Succes msg + test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		flash[:success] = "User created successfully"
+  		flash[:success] = "Welcome, your account has been created successfully"
   		redirect_to @user  			
   	else
   		render 'new'  		
